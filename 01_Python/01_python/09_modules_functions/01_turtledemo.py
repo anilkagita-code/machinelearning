7a40f1abeabbf13afa3cc5da96b7bb74589edb72
Anil Kagita <anilkagita.code@gmail.com>
2021-09-10 01:08:13 +0530

    wip functions and methods

---
+++ 01_Python/01_python/09_modules_functions/01_turtledemo.py
@@ -0,0 +1,11 @@
+import turtle
+from turtle import *
+done = "Completed drawing"
+forward(150)
+right(180)
+forward(250)
+circle(75)
+done
+print(done)
+
+help(turtle)
---
+++ 01_Python/01_python/09_modules_functions/02_browser.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+webbrowser.open('http://www.python.org')
+
+help(webbrowser)
