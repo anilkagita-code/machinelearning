245a9b2d07f0ba909ec24ce7932b04dacbdb292a
@anilkagita-code <anilkagita.code@gmail.com>
2021-10-14 10:48:29 +0530

    working copy logistic regression

--- 01_Python/01_python/09_modules_functions/05_timezones.py
+++ 01_Python/01_python/09_modules_functions/05_timezones.py
@@ -1,6 +1,6 @@
 import datetime
 import time
-
+import dateutil
 import pytz
 
 print(f'The epoch on this system starts at: {time.strftime("%c", time.gmtime(0) )}')
@@ -21,25 +21,36 @@ print(datetime.datetime.today())
 print(datetime.datetime.now())
 print(datetime.datetime.utcnow())
 
-country = "Europe/Moscow"
-country = "IST"
-tz_to_display = pytz.timezone(country)
+country_mos = "Europe/Moscow"
+country_chi = "America/Chicago"
+country_ind = "Asia/Calcutta"
+country_ire = "Europe/Dublin"
+
+
+tz_to_display = pytz.timezone(country_ind)
 print(tz_to_display)
 local_time = datetime.datetime.now(tz=tz_to_display)
-print(f'The time in {country} is {local_time}')
+print(f'The time in {country_ind} is {local_time}')
 print(f'UTC is {datetime.datetime.utcnow()}')
+
+
+tz_to_display = pytz.timezone(country_chi)
+print(tz_to_display)
+local_time = datetime.datetime.now(tz=tz_to_display)
+print(f'The time in {country_chi} is {local_time}')
 
 # The following piece of code shows the problem pytz can give. I was shocked when I first found it out.
 # I.e. the timezone created by pytz is for the true local time, instead of the standard local time people observe.
 # Shanghai conforms to +0800, not +0806 as suggested by pytz:
 
-# print((datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('Asia/Shanghai'))
-#      - datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('UTC')))
-#      .total_seconds())
-# print((datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.gettz('Asia/Shanghai'))
-#      - datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.tzutc()))
-#      .total_seconds())
-#
-# -29160.0
-# -28800.0
+print((datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('Asia/Shanghai')) - datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('UTC'))).total_seconds())
+print((datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.gettz('Asia/Shanghai')) - datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.tzutc())).total_seconds())
+
+
+print((datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('Asia/Shanghai')) - datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('UTC'))).total_seconds())
+print((datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.gettz('Asia/Shanghai')) - datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.tzutc())).total_seconds())
+
+
+print((datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('Asia/Shanghai')) - datetime(2017,2,13,14,29,29, tzinfo=pytz.timezone('UTC'))).total_seconds())
+print((datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.gettz('Asia/Shanghai')) - datetime(2017,2,13,14,29,29, tzinfo=dateutil.tz.tzutc())).total_seconds())
 
