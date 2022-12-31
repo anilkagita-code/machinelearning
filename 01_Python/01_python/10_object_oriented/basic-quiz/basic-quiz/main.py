b03464f1a72f2870eea048c3abd52de1cc6bf3cf
@anilkagita-code <anilkagita.code@gmail.com>
2022-12-31 18:24:57 +0530

    working with OOPS

--- 01_Python/01_python/10_object_oriented/basic-quiz/basic-quiz/main.py
+++ 01_Python/01_python/10_object_oriented/basic-quiz/basic-quiz/main.py
@@ -5,3 +5,13 @@ Created on Sat Dec 31 17:48:15 2022
 @author: anil_
 """
 
+class User:
+    pass
+
+user_1 = User()
+user_1.id = '01'
+user_1.name = 'Ravi'
+
+user_2 = User()
+user_2.id = '02'
+user_2.user_name = 'Rajesh'
